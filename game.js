c56583d6d6565b6a80dc7924e00d39b80a2f1e63
vincentsit-1202 <57486562+vincentsit-1202@users.noreply.github.com>
2019-12-31 17:32:14 +0800

    Update game.js

--- game.js
+++ game.js
@@ -38,21 +38,21 @@ function startGame() {
 
             movebymouse = false
     }, false);
-    //   document.body.addEventListener("touchstart", function (e) {
-    //     if (e.target == canvas) {
-    //       e.preventDefault();
-    //     }
-    //   }, false);
-    //   document.body.addEventListener("touchend", function (e) {
-    //     if (e.target == canvas) {
-    //       e.preventDefault();
-    //     }
-    //   }, false);
-    //   document.body.addEventListener("touchmove", function (e) {
-    //     if (e.target == canvas) {
-    //       e.preventDefault();
-    //     }
-    //   }, false);
+      document.body.addEventListener("touchstart", function (e) {
+        if (e.target == canvas) {
+          e.preventDefault();
+        }
+      }, false);
+      document.body.addEventListener("touchend", function (e) {
+        if (e.target == canvas) {
+          e.preventDefault();
+        }
+      }, false);
+      document.body.addEventListener("touchmove", function (e) {
+        if (e.target == canvas) {
+          e.preventDefault();
+        }
+      }, false);
     myGameArea.canvas.addEventListener("touchstart", function (e) {
         mousePos = getTouchPos(myGameArea.canvas, e);
         
